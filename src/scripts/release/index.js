19b6eef4b496b7d06f1199edb7b2e5e883ef657e
chenxuhua <chenxvhua@mobanker.com>
2017-12-28 18:07:36 +0800

    feat(all):第四次测试

--- src/scripts/release/index.js
+++ src/scripts/release/index.js
@@ -49,7 +49,7 @@ else {
         }
         //major, minor, patch  premajor  preminor prepatch
         let packageObj = require("../../../package.json");
-        const newVersion = semver.inc(packageObj.version,tag,"")
+        const newVersion = semver.inc(packageObj.version,'prerelease',tag)
         console.log('你选择的发布版本', answers.publishVersion,newVersion);
         execa.shellSync("npm version " + newVersion);
 
