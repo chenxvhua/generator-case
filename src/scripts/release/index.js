5639caa80242aa1dfa520cd20ef26b61f159b966
chenxuhua <chenxvhua@mobanker.com>
2017-12-28 19:24:34 +0800

    feat(all):第四次测试

--- src/scripts/release/index.js
+++ src/scripts/release/index.js
@@ -56,7 +56,7 @@ else {
         packageObj.scripts={};
         fs.writeFileSync("build/package.json", JSON.stringify(packageObj, null, 2), "utf8");
         execa.shellSync('git add *');
-        //execa.shellSync('git commit -m "' + packageObj.version + '"');
+        execa.shellSync('git commit -m "' + packageObj.version + '"');
 
         //git push origin master
        // execa.shellSync("git  origin master --follow-tags")
