6224ffff35df879caec592f05d7b775cf62e4bdc
chenxuhua <chenxvhua@mobanker.com>
2017-12-28 19:27:18 +0800

    feat(all):第四次测试

--- src/scripts/release/index.js
+++ src/scripts/release/index.js
@@ -55,11 +55,11 @@ else {
         packageObj.devDependencies={};
         packageObj.scripts={};
         fs.writeFileSync("build/package.json", JSON.stringify(packageObj, null, 2), "utf8");
-        execa.shellSync('git add *');
-        execa.shellSync('git commit -m "' + packageObj.version + '"');
+       // execa.shellSync('git add *');
+       // execa.shellSync('git commit -m "' + packageObj.version + '"');
 
         //git push origin master
-       // execa.shellSync("git  origin master --follow-tags")
+        execa.shellSync("git  push --follow-tags")
         console.log("master分支执行完成");
        // execa.shellSync('cd build && npm publish');
         console.log("发布成功")
