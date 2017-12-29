6996cda756be2b068d11d157b63ba40583012fe7
chenxuhua <chenxvhua@mobanker.com>
2017-12-29 19:41:13 +0800

    feat(all):工具类检查源代码总行数

---
+++ src/module/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by chenxuhua on 2017/9/6.
+ */
+
+import statisticsLine from "./statistics_line/index.js"
+const path = require("path")
+statisticsLine({workDic:path.resolve(process.cwd(),"src/scripts")})
+
+
+
+
+
+
+
+
+
